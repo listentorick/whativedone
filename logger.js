c156b46634cfae8fa0233e88de7d5b7f72bccf8c
Rick Walsh <Rick.Walsh@gmail.com>
2013-04-04 17:27:55 +0100

    logger now logs uncaught exceptions

--- logger.js
+++ logger.js
@@ -3,15 +3,15 @@ require('winston-papertrail').Papertrail;
 
 var logger = new (winston.Logger)({
   transports: [
-	new (winston.transports.Papertrail)({ host: 'logs.papertrailapp.com',port: '52777'}),
-    new (winston.transports.Console)({ json: false, timestamp: true }),
-    new winston.transports.File({ filename: __dirname + '/debug.log', json: false }),
+	new (winston.transports.Papertrail)({ host: 'logs.papertrailapp.com',port: '52777', handleExceptions: true}),
+    new (winston.transports.Console)({ json: false, timestamp: true, handleExceptions: true}),
+    //new winston.transports.File({ filename: __dirname + '/debug.log', json: false }),
 	
-  ],
-  exceptionHandlers: [
-    new (winston.transports.Console)({ json: false, timestamp: true }),
-    new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
-  ],
+  ]//,
+  //exceptionHandlers: [
+   // new (winston.transports.Console)({ json: false, timestamp: true }),
+    //new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
+  //],
   exitOnError: false
 });
 
